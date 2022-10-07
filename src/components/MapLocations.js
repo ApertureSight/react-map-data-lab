82d9f8cf90e2a1ca21b4a26e050b052abfe9421a
ApertureSight <97801261+ApertureSight@users.noreply.github.com>
2022-10-06 22:07:07 -0400

    Map Lab Commit

---
+++ src/components/MapLocations.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import "./MapStyles.css";
+
+export default function MapLocation(props) {
+  return (
+    <circle
+      className="location"
+      cx={props.position.x}
+      cy={props.position.y}
+      onClick={props.userSelected}
+      r="12"
+    />
+  );
+}
